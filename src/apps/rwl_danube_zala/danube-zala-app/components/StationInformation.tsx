8e2dac899bcfb10bd86952067b04496b3bd314b8
Martin Pontius <m.pontius@52north.org>
2025-03-28 16:54:13 +0100

    Fix file name and explanation text

--- src/apps/rwl_danube_zala/danube-zala-app/components/StationInformation.tsx
+++ src/apps/rwl_danube_zala/danube-zala-app/components/StationInformation.tsx
@@ -2,37 +2,33 @@
 // SPDX-License-Identifier: Apache-2.0
 
 interface StationData {
-    settlement: string; 
-    adress: string; 
-    type: string; 
-    eventType: string; 
-    locationType: string; 
-    date: string; 
-    damageType: string; 
-    county: string; 
+    settlement: string;
+    adress: string;
+    type: string;
+    eventType: string;
+    locationType: string;
+    date: string;
+    damageType: string;
+    county: string;
 }
 
-
-const StationInformation = (data:StationData) => {
-    const stationData:StationData  = data.data; 
-    if (Object.keys(stationData).length == 0){
-        return <>
-            Select a station on the map
-        </>;
+const StationInformation = (data: StationData) => {
+    const stationData: StationData = data.data;
+    if (Object.keys(stationData).length == 0) {
+        return <>Select an event on the map</>;
     }
-    return <>
-        <div>Cím: {stationData.adress}</div>
-        <div>Település: {stationData.settlement}</div>
-        <div>Beavatkozás típusa: {stationData.type}</div>
-        <div>Esemény típus: {stationData.eventType}</div>
-        <div>Helyszín típusa: {stationData.locationType}</div>
-        <div>Jelzés dátuma: {stationData.date}</div>
-        <div>Káreset fajtája: {stationData.damageType}</div>
-        <div>Megye (mk.): {stationData.county}</div>
-
-
-
-    </>;
+    return (
+        <>
+            <div>Cím: {stationData.adress}</div>
+            <div>Település: {stationData.settlement}</div>
+            <div>Beavatkozás típusa: {stationData.type}</div>
+            <div>Esemény típus: {stationData.eventType}</div>
+            <div>Helyszín típusa: {stationData.locationType}</div>
+            <div>Jelzés dátuma: {stationData.date}</div>
+            <div>Káreset fajtája: {stationData.damageType}</div>
+            <div>Megye (mk.): {stationData.county}</div>
+        </>
+    );
 };
 
-export default StationInformation; 
+export default StationInformation;
