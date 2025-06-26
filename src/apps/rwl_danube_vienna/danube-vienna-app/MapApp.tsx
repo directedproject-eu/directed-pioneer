e42733849dea7f5d443445ba3aee6598ad968b25
Martin Pontius <m.pontius@52north.org>
2025-06-26 11:12:56 +0200

    Group Vienna layers

--- src/apps/rwl_danube_vienna/danube-vienna-app/MapApp.tsx
+++ src/apps/rwl_danube_vienna/danube-vienna-app/MapApp.tsx
@@ -164,8 +164,8 @@ export function MapApp() {
                                 <Toc
                                     mapId={MAP_ID}
                                     showTools={true}
-                                    // collapsibleGroups={true}
-                                    // initiallyCollapsed={true}
+                                    collapsibleGroups={true}
+                                    initiallyCollapsed={true}
                                     showBasemapSwitcher={false}
                                 />
                                 <FormControl>
--- src/apps/rwl_danube_vienna/danube-vienna-app/services.ts
+++ src/apps/rwl_danube_vienna/danube-vienna-app/services.ts
@@ -1,13 +1,70 @@
 // SPDX-FileCopyrightText: 2023-2025 Open Pioneer project (https://github.com/open-pioneer)
 // SPDX-License-Identifier: Apache-2.0
-import { MapConfig, MapConfigProvider, SimpleLayer } from "@open-pioneer/map";
+import { GroupLayer, MapConfig, MapConfigProvider, SimpleLayer } from "@open-pioneer/map";
 import TileLayer from "ol/layer/Tile";
 import TileWMS from "ol/source/TileWMS";
 import OSM from "ol/source/OSM";
 
+
+const wmsLayersHistorical = [
+    {
+        "name": "WD_RAIN172645",
+        "title": "WD_RAIN172645",
+        "description": "Water depth caused by pluvial flooding"
+    },
+    {
+        "name": "WD_RAIN110828",
+        "title": "WD_RAIN110828",
+        "description": "Water depth caused by pluvial flooding"
+    },
+    {
+        "name": "WD_RAIN095830",
+        "title": "WD_RAIN095830",
+        "description": "Water depth caused by pluvial flooding"
+    },
+    {
+        "name": "DMG_RIVER111745",
+        "title": "DMG_RIVER111745",
+        "description": "Damage caused by fluvial flooding"
+    },
+    {
+        "name": "DMG_RAIN110828",
+        "title": "DMG_RAIN110828",
+        "description": "Damage caused by pluvial flooding"
+    }
+];
+
 export const MAP_ID = "main";
 export class MainMapProvider implements MapConfigProvider {
     mapId = MAP_ID;
+
+    createWmsLayer(layerName: string, layerTitle: string, layerDescription: string) {
+        const wmsLayer = new SimpleLayer({
+            id: layerName,
+            title: layerTitle,
+            description: layerDescription,
+            visible: false,
+            olLayer: new TileLayer({
+                source: new TileWMS({
+                    url: "https://directed.dev.52north.org/geoserver/directed/wms",
+                    params: {
+                        LAYERS: layerName
+                    }
+                }),
+                properties: {
+                    title: layerTitle,
+                    id: layerName
+                }
+            }),
+            // attributes: {
+            //     "legend": {
+            //         Component: WaterLevelLegend
+            //     }
+            // },
+            isBaseLayer: false
+        });
+        return wmsLayer;
+    }
 
     async getMapConfig(): Promise<MapConfig> {
         return {
@@ -18,6 +75,17 @@ export class MainMapProvider implements MapConfigProvider {
             },
             projection: "EPSG:3857",
             layers: [
+                // GROUP HISTORICAL LAYERS
+                new GroupLayer({
+                    title: "Historical Layers",
+                    visible: false,
+                    id: "historical",
+                    layers: [
+                        ...wmsLayersHistorical.map(({ name, title, description }) =>
+                            this.createWmsLayer(name, title, description)
+                        )
+                    ]
+                }),
                 new SimpleLayer({
                     title: "OpenStreetMap",
                     olLayer: new TileLayer({
@@ -25,111 +93,6 @@ export class MainMapProvider implements MapConfigProvider {
                         properties: { title: "OSM" }
                     }),
                     isBaseLayer: true
-                }),
-
-                //WMS layer
-                new SimpleLayer({
-                    id: "WD_RAIN172645",
-                    title: "WD_RAIN172645",
-                    description: "WD_RAIN172645",
-                    olLayer: new TileLayer({
-                        source: new TileWMS({
-                            url: "https://directed.dev.52north.org/geoserver/directed/wms",
-                            params: {
-                                LAYERS: "WD_RAIN172645"
-                            }
-                        }),
-                        properties: {
-                            title: "WD_RAIN172645",
-                            id: "WD_RAIN172645"
-                        }
-                    }),
-                    isBaseLayer: false,
-                    visible: true
-                }),
-
-                //WMS layer
-                new SimpleLayer({
-                    id: "WD_RAIN110828",
-                    title: "WD_RAIN110828",
-                    description: "WD_RAIN110828",
-                    olLayer: new TileLayer({
-                        source: new TileWMS({
-                            url: "https://directed.dev.52north.org/geoserver/directed/wms",
-                            params: {
-                                LAYERS: "WD_RAIN110828"
-                            }
-                        }),
-                        properties: {
-                            title: "WD_RAIN110828",
-                            id: "WD_RAIN110828"
-                        }
-                    }),
-                    isBaseLayer: false,
-                    visible: false
-                }),
-
-                //WMS layer
-                new SimpleLayer({
-                    id: "WD_RAIN095830",
-                    title: "WD_RAIN095830",
-                    description: "WD_RAIN095830",
-                    olLayer: new TileLayer({
-                        source: new TileWMS({
-                            url: "https://directed.dev.52north.org/geoserver/directed/wms",
-                            params: {
-                                LAYERS: "WD_RAIN095830"
-                            }
-                        }),
-                        properties: {
-                            title: "WD_RAIN095830",
-                            id: "WD_RAIN095830"
-                        }
-                    }),
-                    isBaseLayer: false,
-                    visible: false
-                }),
-
-                //WMS layer
-                new SimpleLayer({
-                    id: "DMG_RIVER111745",
-                    title: "DMG_RIVER111745",
-                    description: "DMG_RIVER111745",
-                    olLayer: new TileLayer({
-                        source: new TileWMS({
-                            url: "https://directed.dev.52north.org/geoserver/directed/wms",
-                            params: {
-                                LAYERS: "DMG_RIVER111745"
-                            }
-                        }),
-                        properties: {
-                            title: "DMG_RIVER111745",
-                            id: "DMG_RIVER111745"
-                        }
-                    }),
-                    isBaseLayer: false,
-                    visible: false
-                }),
-
-                //WMS layer
-                new SimpleLayer({
-                    id: "DMG_RAIN110828",
-                    title: "DMG_RAIN110828",
-                    description: "DMG_RAIN110828",
-                    olLayer: new TileLayer({
-                        source: new TileWMS({
-                            url: "https://directed.dev.52north.org/geoserver/directed/wms",
-                            params: {
-                                LAYERS: "DMG_RAIN110828"
-                            }
-                        }),
-                        properties: {
-                            title: "DMG_RAIN110828",
-                            id: "DMG_RAIN110828",
-                        }
-                    }),
-                    isBaseLayer: false,
-                    visible: false
                 })
             ]
         };
